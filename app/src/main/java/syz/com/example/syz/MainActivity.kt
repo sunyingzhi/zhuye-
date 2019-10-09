f84bd9d78b9cf42dc1944c9eeaf6f1c15264e6c3
DL <1712979032@qq.com>
2019-10-09 14:06:42 +0800

    作业

---
+++ app/src/main/java/syz/com/example/syz/MainActivity.kt
@@ -0,0 +1,12 @@
+package syz.com.example.syz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
